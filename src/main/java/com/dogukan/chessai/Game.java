8392b36d3e59ebe1137e9ac90865a8ea2065a954
Kartal <k1921573@nmslab-004121.nms.kcl.ac.uk>
2021-10-25 19:15:23 +0100

    gui for chess squares

--- src/main/java/com/dogukan/chessai/Game.java
+++ src/main/java/com/dogukan/chessai/Game.java
@@ -2,7 +2,13 @@ package com.dogukan.chessai;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Game extends Application {
@@ -11,8 +17,24 @@ public class Game extends Application {
     public void start(Stage stage) throws Exception {
         stage.setTitle("Chess");
 
-        StackPane layout = new StackPane();
-        Scene scene = new Scene(layout, 500, 500);
+        VBox vBox = new VBox();
+        Color currentColour = Color.BLACK;
+        for (int i = 0; i < 8; i++) {
+            HBox hBox = new HBox();
+            for (int j = 0; j < 8; j++) {
+                Rectangle square = new Rectangle(10, 10);
+                if (currentColour == Color.BLACK) {
+                    currentColour = Color.GREEN;
+                } else {
+                    currentColour = Color.BLACK;
+                }
+                square.setFill(currentColour);
+                hBox.getChildren().add(square);
+            }
+            vBox.getChildren().add(hBox);
+        }
+
+        Scene scene = new Scene(vBox, 500, 500);
 
         stage.setScene(scene);
         stage.show();
