e34356e39c83eb6e1d58d56f08fbd5b0a91e8a70
Dogukan Kartal <dogukan_kartal98@hotmail.com>
2021-11-11 13:02:51 +0000

    implement all legal moves for pawn

--- src/main/java/com/dogukan/chessai/chess/Pawn.java
+++ src/main/java/com/dogukan/chessai/chess/Pawn.java
@@ -7,10 +7,12 @@ import java.util.Set;
 public class Pawn extends Piece {
 
     private boolean firstMove;
+    private boolean tookTwoSquareMove;
 
     Pawn(PieceColour colour) {
         super(colour);
         firstMove = true;
+        tookTwoSquareMove = false;
     }
 
     @Override
@@ -21,44 +23,83 @@ public class Pawn extends Piece {
         Optional<Move> twoSquareMove = twoSquareMove(board, position);
         Optional<Move> takeLeft = takeLeft(board, position);
         Optional<Move> takeRight = takeRight(board, position);
+        Optional<Move> enPassantLeft = enPassantLeft(board, position);
+        Optional<Move> enPassantRight = enPassantRight(board, position);
 
         oneSquareMove.ifPresent(legalMoves::add);
         twoSquareMove.ifPresent(legalMoves::add);
         takeLeft.ifPresent(legalMoves::add);
         takeRight.ifPresent(legalMoves::add);
+        enPassantLeft.ifPresent(legalMoves::add);
+        enPassantRight.ifPresent(legalMoves::add);
 
         return legalMoves;
     }
 
-//        boolean pawnOnLeft;
-//        boolean pawnOnRight;
-//
-//        if (super.getColour() == PieceColour.WHITE) {
-//            takeLeft = getBoard().getSquare(currentX - 1, currentY + 1);
-//            takeRight = getBoard().getSquare(currentX + 1, currentY + 1);
-//            pawnOnLeft = getBoard().getSquare(currentX - 1, currentY).getCurrentPiece() instanceof Pawn;
-//            pawnOnRight = getBoard().getSquare(currentX + 1, currentY).getCurrentPiece() instanceof Pawn;
-//        } else {
-//            takeLeft = getBoard().getSquare(currentX + 1, currentY - 1);
-//            takeRight = getBoard().getSquare(currentX - 1, currentY - 1);
-//            pawnOnLeft = getBoard().getSquare(currentX + 1, currentY).getCurrentPiece() instanceof Pawn;
-//            pawnOnRight = getBoard().getSquare(currentX - 1, currentY).getCurrentPiece() instanceof Pawn;
-//        }
-//        if (!takeLeft.isEmpty() || pawnOnLeft && !takeLeft.isEmpty()) {
-//            legalMoves.add(takeLeft);
-//        }
-//        if (!takeRight.isEmpty() || pawnOnRight && takeRight.isEmpty()) {
-//            legalMoves.add(takeRight);
-//        }
-//        return legalMoves;
-//    }
+    private Optional<Move> enPassantRight(Board board, Position position) {
+        int currentX = position.getX();
+        int currentY = position.getY();
+        Piece piece;
+
+        if (super.getColour() == PieceColour.WHITE) {
+            piece = board.getSquare(new Position(currentX + 1, currentY));
+        } else {
+            piece = board.getSquare(new Position(currentX - 1, currentY));
+        }
+        if ((piece instanceof Pawn) && ((Pawn) piece).tookTwoSquareMove) {
+            if (getColour() == PieceColour.WHITE) {
+                if (board.isEmpty(new Position(currentX + 1, currentY + 1))) {
+                    return Optional.of(new Move(position, new Position(currentX + 1, currentY + 1)));
+                } else {
+                    return Optional.empty();
+                }
+            } else {
+                if (board.isEmpty(new Position(currentX - 1, currentY - 1))) {
+                    return Optional.of(new Move(position, new Position(currentX - 1, currentY - 1)));
+                } else {
+                    return Optional.empty();
+                }
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private Optional<Move> enPassantLeft(Board board, Position position) {
+        int currentX = position.getX();
+        int currentY = position.getY();
+        Piece piece;
+
+        if (super.getColour() == PieceColour.WHITE) {
+            piece = board.getSquare(new Position(currentX - 1, currentY));
+        } else {
+            piece = board.getSquare(new Position(currentX + 1, currentY));
+        }
+        if ((piece instanceof Pawn) && ((Pawn) piece).tookTwoSquareMove) {
+            if (getColour() == PieceColour.WHITE) {
+                if (board.isEmpty(new Position(currentX - 1, currentY + 1))) {
+                    return Optional.of(new Move(position, new Position(currentX - 1, currentY + 1)));
+                } else {
+                    return Optional.empty();
+                }
+            } else {
+                if (board.isEmpty(new Position(currentX + 1, currentY - 1))) {
+                    return Optional.of(new Move(position, new Position(currentX + 1, currentY - 1)));
+                } else {
+                    return Optional.empty();
+                }
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
 
     private Optional<Move> takeLeft(Board board, Position position) {
         int currentX = position.getX();
         int currentY = position.getY();
         Move takeLeft;
 
-        if (super.getColour() == PieceColour.WHITE) {
+        if (getColour() == PieceColour.WHITE) {
             takeLeft = new Move(position, new Position(currentX - 1, currentY + 1));
         } else {
             takeLeft = new Move(position, new Position(currentX + 1, currentY - 1));
@@ -76,7 +117,7 @@ public class Pawn extends Piece {
         int currentY = position.getY();
         Move takeRight;
 
-        if (super.getColour() == PieceColour.WHITE) {
+        if (getColour() == PieceColour.WHITE) {
             takeRight = new Move(position, new Position(currentX + 1, currentY + 1));
         } else {
             takeRight = new Move(position, new Position(currentX - 1, currentY - 1));
@@ -114,7 +155,7 @@ public class Pawn extends Piece {
         Move twoSquareMove;
 
         if (firstMove) {
-            if (super.getColour() == PieceColour.WHITE) {
+            if (getColour() == PieceColour.WHITE) {
                 twoSquareMove = new Move(position, new Position(currentX, currentY + 2));
             } else {
                 twoSquareMove = new Move(position, new Position(currentX, currentY - 2));
