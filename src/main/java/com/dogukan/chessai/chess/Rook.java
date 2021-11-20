870027ec280bc33ad923dfb5713eecc5870fbaca
Dogukan Kartal <dogukan_kartal98@hotmail.com>
2021-11-20 22:43:47 +0000

    implement vertical move for rook

--- src/main/java/com/dogukan/chessai/chess/Rook.java
+++ src/main/java/com/dogukan/chessai/chess/Rook.java
@@ -9,13 +9,41 @@ public class Rook extends Piece {
 
     @Override
     public void legalMoves(Board board, Position position) {
-
+        horizontalMove(board, position);
+        verticalMove(board, position);
     }
 
     private void horizontalMove(Board board, Position position) {
 
         emptySquareOnLeft(board, position);
         emptySquareOnRight(board, position);
+    }
+
+    private void verticalMove(Board board, Position position) {
+        emptySquareOnAbove(board, position);
+        emptySquareBelow(board, position);
+    }
+
+    private void emptySquareOnAbove(Board board, Position position) {
+        int currentX = position.getX();
+        int currentY = position.getY();
+
+        int i = 1;
+        while (board.isEmpty(new Position(currentX, currentY - 1))) {
+            getLegalMoves().add(new Move(position, new Position(currentX, currentY - i)));
+            i++;
+        }
+    }
+
+    private void emptySquareBelow(Board board, Position position) {
+        int currentX = position.getX();
+        int currentY = position.getY();
+
+        int i = 1;
+        while (board.isEmpty(new Position(currentX, currentY + 1))) {
+            getLegalMoves().add(new Move(position, new Position(currentX, currentY + i)));
+            i++;
+        }
     }
 
     private void emptySquareOnLeft(Board board, Position position) {
