ec5a95a4f5fb966c61de1a621fe63cfaa0bc0e8a
Dogukan Kartal <dogukan_kartal98@hotmail.com>
2022-01-19 15:33:25 +0000

    reimplement rook legal moves

--- src/main/java/com/dogukan/chessai/chess/Rook.java
+++ src/main/java/com/dogukan/chessai/chess/Rook.java
@@ -2,14 +2,15 @@ package com.dogukan.chessai.chess;
 
 public class Rook extends Piece {
 
-
     Rook(PieceColour colour) {
         super(colour);
     }
 
     @Override
     public void legalMoves(Board board, Position position) {
-//        getLegalMoves().addAll(Direction.horizontalMove(board, position, getColour()));
-//        getLegalMoves().addAll(Direction.verticalMove(board, position, getColour()));
+        getLegalMoves().addAll(Direction.eastMove(board, position, getColour(), board.columnLength()));
+        getLegalMoves().addAll(Direction.westMove(board, position, getColour(), board.columnLength()));
+        getLegalMoves().addAll(Direction.northMove(board, position, getColour(), board.columnLength()));
+        getLegalMoves().addAll(Direction.southMove(board, position, getColour(), board.columnLength()));
     }
 }
