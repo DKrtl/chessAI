cf41a95a439e2f8b464286e6bdffdf48821f16a5
Dogukan Kartal <dogukan_kartal98@hotmail.com>
2022-01-19 15:26:55 +0000

    remove first row of pieces from both sides

--- src/main/java/com/dogukan/chessai/chess/Game.java
+++ src/main/java/com/dogukan/chessai/chess/Game.java
@@ -12,11 +12,12 @@ public class Game {
         Board board = new Board(new Piece[8][8]);
         for(int i = 0; i < 8; i++) {
             for(int j = 0; j < 8; j++) {
-                if (j == 1) {
-                    board.addPiece(new Position(i, j), new Pawn(PieceColour.BLACK));
-                } else if (j == 6) {
-                    board.addPiece(new Position(i, j), new Pawn(PieceColour.WHITE));
-                } else if (j == 0) {
+//                if (j == 1) {
+//                    board.addPiece(new Position(i, j), new Pawn(PieceColour.BLACK));
+//                } else if (j == 6) {
+//                    board.addPiece(new Position(i, j), new Pawn(PieceColour.WHITE));
+//                } else
+                if (j == 0) {
                     if (i == 0 || i == 7) {
                         board.addPiece(new Position(i , j), new Rook(PieceColour.BLACK));
                     } else if (i == 1 || i == 6) {
