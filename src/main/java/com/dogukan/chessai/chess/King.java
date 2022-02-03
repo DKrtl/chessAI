8a002ef5e3725e1cf4bd53770ff4b2c2f214e0c5
Dogukan Kartal <dogukan_kartal98@hotmail.com>
2022-02-03 18:19:31 +0000

    re add knight and king legal moves

--- src/main/java/com/dogukan/chessai/chess/King.java
+++ src/main/java/com/dogukan/chessai/chess/King.java
@@ -8,15 +8,13 @@ public class King extends Piece {
 
     @Override
     public void legalMoves(Board board, Position position) {
-//        int currentX = position.getX();
-//        int currentY = position.getY();
-//
-//        getLegalMoves().add(new Move(position, new Position(currentX, currentY + 1)));
-//        getLegalMoves().add(new Move(position, new Position(currentX + 1, currentY + 1)));
-//        getLegalMoves().add(new Move(position, new Position(currentX + 1, currentY)));
-//        getLegalMoves().add(new Move(position, new Position(currentX + 1, currentY - 1)));
-//        getLegalMoves().add(new Move(position, new Position(currentX, currentY - 1)));
-//        getLegalMoves().add(new Move(position, new Position(currentX - 1, currentY - 1)));
-//        getLegalMoves().add(new Move(position, new Position(currentX - 1, currentY)));
+        getLegalMoves().addAll(Direction.northMove(board, position, getColour(), 1));
+        getLegalMoves().addAll(Direction.northEastMove(board, position, getColour(), 1));
+        getLegalMoves().addAll(Direction.eastMove(board, position, getColour(), 1));
+        getLegalMoves().addAll(Direction.southEastMove(board, position, getColour(), 1));
+        getLegalMoves().addAll(Direction.southMove(board, position, getColour(), 1));
+        getLegalMoves().addAll(Direction.southWestMove(board, position, getColour(), 1));
+        getLegalMoves().addAll(Direction.westMove(board, position, getColour(), 1));
+        getLegalMoves().addAll(Direction.northWestMove(board, position, getColour(), 1));
     }
 }
--- src/main/java/com/dogukan/chessai/chess/Knight.java
+++ src/main/java/com/dogukan/chessai/chess/Knight.java
@@ -8,18 +8,18 @@ public class Knight extends Piece {
 
     @Override
     public void legalMoves(Board board, Position position) {
-//        int currentX = position.getX();
-//        int currentY = position.getY();
-//
-//        Position northEast = new Position(currentX + 1, currentY - 2);
-//        Position southEast = new Position(currentX + 1, currentY + 2);
-//        Position southWest = new Position(currentX - 1, currentY + 2);
-//        Position northWest = new Position(currentX - 1, currentY - 2);
-//
-//        createMove(board, new Move(position, northEast));
-//        createMove(board, new Move(position, southEast));
-//        createMove(board, new Move(position, southWest));
-//        createMove(board, new Move(position, northWest));
+        int currentX = position.getX();
+        int currentY = position.getY();
+
+        Position northEast = new Position(currentX + 1, currentY - 2);
+        Position southEast = new Position(currentX + 1, currentY + 2);
+        Position southWest = new Position(currentX - 1, currentY + 2);
+        Position northWest = new Position(currentX - 1, currentY - 2);
+
+        createMove(board, new Move(position, northEast));
+        createMove(board, new Move(position, southEast));
+        createMove(board, new Move(position, southWest));
+        createMove(board, new Move(position, northWest));
     }
 
     private void createMove(Board board, Move move) {
